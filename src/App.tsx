fec61ed7eb46efa0552dd91b4691ee5585c97284
Varshan0803 <varshanraman0803@gmail.com>
2026-01-10 01:46:53 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // <--- CHANGED THIS IMPORT
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,14 +13,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* CHANGED: Added basename so React knows it's in a subfolder */}
-      <BrowserRouter basename="/portfolio-pro">
+      {/* CHANGED: Using HashRouter (Fail-proof for GitHub Pages) */}
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
